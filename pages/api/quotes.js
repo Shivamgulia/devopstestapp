cb015f220c8e49f68518feb6cc2655317f685a50
shivamgulia <shivam.meert@gmail.com>
2024-10-10 10:35:27 +0530

    spelling fix

--- pages/api/quotes.js
+++ pages/api/quotes.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
     console.log(userid, quote);
 
     if (!author) {
-      author = "Aclenonymous";
+      author = "Anonymous";
     }
 
     try {
